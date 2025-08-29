2bc4aea19d3568d33913eca79c18e8d347e0da33
Can Kolay <budchirp@gmail.com>
2025-08-29 15:52:08 +0300

    chore: add loading prop to button component

--- packages/components/src/components/button/button.tsx
+++ packages/components/src/components/button/button.tsx
@@ -4,16 +4,25 @@ import { buttonStyle } from '@/components/button/button.style'
 import { cn } from '@/utils/cn'
 
 import type { ButtonProps } from '@/components/button/types'
+import { LoaderCircle } from 'lucide-react'
 
 export const Button: React.FC<ButtonProps> = ({
   children,
   className,
   shape,
   color,
+  disabled,
+  loading = false,
   ...props
 }: ButtonProps): Children => {
   return (
-    <button {...props} className={cn(buttonStyle({ className, shape, color }))}>
+    <button
+      {...props}
+      disabled={loading || disabled}
+      className={cn(buttonStyle({ className, shape, color }))}
+    >
+      {loading && <LoaderCircle className={cn('size-8 animate-spin', children && 'mr-2')} />}
+
       {children}
     </button>
   )
--- packages/components/src/components/button/types.tsx
+++ packages/components/src/components/button/types.tsx
@@ -7,4 +7,6 @@ import type { buttonStyle } from '@/components/button/button.style'
 export type ButtonProps = ComponentProps<'button'> &
   VariantProps<typeof buttonStyle> & {
     children?: Children
+  } & {
+    loading?: boolean
   }
